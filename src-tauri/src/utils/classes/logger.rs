6a2fba85fc20c5ffe14e95ae19db3100645a729f
MMinusOne <michael.business.lb@proton.me>
2025-08-02 12:31:32 +0300

    Logger singleton

--- src-tauri/src/utils/classes/logger.rs
+++ src-tauri/src/utils/classes/logger.rs
@@ -1,10 +1,23 @@
+use std::sync::{LazyLock, OnceLock};
+
 pub struct CommunicationLogger;
 
 //TODO: frontend message
 //TODO: singleton pattern
 
 impl CommunicationLogger {
-    pub fn success(&self, message: &str) {}
+   pub fn instance() -> &'static CommunicationLogger {
+        static INSTANCE: OnceLock<CommunicationLogger> = OnceLock::new();
+        return INSTANCE.get_or_init(|| CommunicationLogger::new());
+    }
+
+    pub fn new() -> Self { 
+        return Self;
+    }
+
+    pub fn success(&self, message: &str) {
+        println!("{:?}", message);
+    }
 
     pub fn warning(&self, message: &str) {}
 
@@ -15,4 +28,4 @@ impl CommunicationLogger {
     }
 }
 
-pub static LOGGER: CommunicationLogger = CommunicationLogger;
+pub static LOGGER : LazyLock<&CommunicationLogger>= LazyLock::new(|| CommunicationLogger::instance());

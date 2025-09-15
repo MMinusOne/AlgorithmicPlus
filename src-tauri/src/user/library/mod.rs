8ce248719c7225245edf1130390fde2e15c9d689
MMinusOne <michael.business.lb@proton.me>
2025-09-15 12:34:22 +0300

    Injectable cleanup

--- src-tauri/src/user/library/mod.rs
+++ src-tauri/src/user/library/mod.rs
@@ -14,39 +14,3 @@ pub trait IInjectable<Input: for<'de> Deserialize<'de>, Output: for<'de> Deseria
 
 pub mod injectables;
 pub use injectables::*;
-
-#[derive(Clone)]
-pub enum Injectable {
-    FloatNumericType(&'static dyn IInjectable<f32, f32>),
-    TupleFloatNumericType(&'static dyn IInjectable<f32, (f32, f32)>),
-    IntegerNumericType(&'static dyn IInjectable<i64, f32>),
-    TradeNumericType(&'static dyn IInjectable<Trade, f32>),
-    TupleIntegerType(&'static dyn IInjectable<f32, (i32, i32)>), // Add any custom types if needed
-}
-
-impl Injectable {
-    pub fn extract_float_numeric(
-        injectable_type: Injectable,
-    ) -> &'static dyn IInjectable<f32, f32> {
-        match injectable_type {
-            Self::FloatNumericType(v) => v,
-            _ => panic!("Invalid compsition type conversion."),
-        }
-    }
-    pub fn extract_integer_numeric(
-        injectable_type: Injectable,
-    ) -> &'static dyn IInjectable<i64, f32> {
-        match injectable_type {
-            Self::IntegerNumericType(v) => v,
-            _ => panic!("Invalid compsition type conversion."),
-        }
-    }
-    pub fn extract_trade_numeric(
-        injectable_type: Injectable,
-    ) -> &'static dyn IInjectable<Trade, f32> {
-        match injectable_type {
-            Self::TradeNumericType(v) => v,
-            _ => panic!("Invalid compsition type conversion."),
-        }
-    }
-}
